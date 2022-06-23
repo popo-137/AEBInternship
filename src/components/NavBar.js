e1804cdda1c4c9188c37f4c90280d75a752eab90
popo-137 <popo_137@mail.ru>
2022-06-23 21:09:57 +0900

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,21 +4,25 @@ import { Container, Nav, Navbar, NavDropdown, } from "react-bootstrap";
 
 const NavBar = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="blue">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                <img
+                    alt=""
+                    src="/unnamed (1).png"
+                    width="60"
+                    height="30"
+                    className="d-inline-block align-top"
+                />{' '}
+                AEB.Internship
+                    </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#info">Общая информация</Nav.Link>
+                        <Nav.Link href="#bankemploee">Сотрудникам банка</Nav.Link>
+                        <Nav.Link href="#students">Студентам</Nav.Link>
+                        <Nav.Link href="#uniemploee">Сотрудникам ВУЗов</Nav.Link>
                     </Nav>
                     <Nav>
                         <Nav.Link href="#deets">More deets</Nav.Link>
