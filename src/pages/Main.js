cdf961f6a6369cd1c56b3e87f26f7c01b74f1be2
popo-137 <popo_137@mail.ru>
2022-06-28 21:44:10 +0900

    main dodelal tipo

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,23 +6,35 @@ const Main = () => {
 
     return (
         <div className="container-sm" align="center">
-                    <h1 className="mt-0 mb-16" >
+            <div
+                className=""
+                style={{
+                    backgroundColor: "#27303E",
+                    backgroundImage:
+                        "url(" + require("../assets/bg3.jpg").default + ")",
+                    backgroundPosition: "center"
+                }}
+            >
+                <div className="" >
+                    <h1 className="text-white pt-5" >
                         Практика студентов 2023
                     </h1>
                     <div className="container-xs">
-                        <p className="m-0 mb-32" >
+                        <p className="m-0 mb-32 text-white" >
                             Для участия в практике нужно подать заявку на участие до 30 мая 2023 года включительно.
                         </p>
-                        <div>
-                                <Button  color="primary"  href={VACANCIES_ROUTE} >
+                        <div className="py-5">
+                                <Button  variant="outline-primary"  href={VACANCIES_ROUTE} >
                                     Посмотреть вакансии
                                 </Button>
-                                <Button  color="dark" className="m-lg-2" href={APPLICATION_ROUTE} >
+                                <Button  variant="outline-primary" className="m-lg-2" href={APPLICATION_ROUTE} >
                                     Подать заявку
                                 </Button>
 
                         </div>
                     </div>
+                </div>
+            </div>
             <Row className="mt-5">
                 <Col><h5>Новости и события</h5>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
